0b08de11271afeca86ff37da46a6ff1d72d38f01
Emre Guneyler <eguneys@hotmail.com>
2022-03-14 03:07:57 +0000

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+import typescript from '@rollup/plugin-typescript'
+import css from 'rollup-plugin-import-css'
+
+import htmlTemplate from 'rollup-plugin-generate-html-template'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+import { terser }  from 'rollup-plugin-terser'
+
+
+export default args => {
+  let prod = args['config-prod']
+
+  return {
+    input: 'src/main.ts',
+    output: {
+      format: 'iife',
+      name: 'Solsido',
+      dir: 'dist',
+      ...(prod ? {
+        entryFileNames: '[name].min.js',
+        plugins: [terser({mangle: { properties: { keep_quoted: true } } })]
+      } : { sourcemap: true })
+    },
+    watch: {
+      clearScreen: true
+    },
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      css(),
+      htmlTemplate({
+        template: 'src/index.html',
+        target: 'index.html'
+      }),
+      ...(prod? [] : [
+        serve({ contentBase: 'dist', port: 3000 }),
+        livereload({ watch: 'dist', port: 8080 })
+      ])
+    ]
+
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+type Config = {}
+
+export default function Solsido(element: HTMLElement, config: Config) {
+  console.log('here')
+}
